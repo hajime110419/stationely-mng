a415c9ea142492140b634b319797e7a7cd3ef7a8
Hajime Kugimiya <hajime110419@gmail.com>
2026-01-09 22:56:25 +0900

    Stationery struct implementation

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is Stationery maneger.Hello, World!")
+}
+
+type Stationery struct {
+	ID        string    `json:"id"`
+	Category  string    `json:"category"`
+	Name      string    `json:"name"`
+	Location  string    `json:"location"`
+	InUse     bool      `json:"in_use"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func main() {
